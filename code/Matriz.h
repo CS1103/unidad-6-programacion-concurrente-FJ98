12f9d4723d29b5d23fa5f0c7c35e73ae9cf360ae
FJ98 <felix.solano@utec.edu.pe>
2019-06-22 20:20:18 -0500

    Proyecto Grupal

--- code/Matriz.h
+++ code/Matriz.h
@@ -3,6 +3,7 @@
 #define CODE_MATRIZ_H
 
 #include <iostream>
+#include <random>
 
 using namespace std;
 
@@ -14,13 +15,13 @@ class Matriz{
 public:
     Matriz():filas{0}, columnas{0} {};
     Matriz(T filas, T columnas):filas{filas}, columnas{columnas} {
-        matriz = new T* [filas];
-        for (int i = 0; i < filas; ++i)
+        matriz = new T *[filas];
+        for (int i = 0; i < filas; ++i){
             matriz[i] = new T[columnas];
-
+        }
         for (int i = 0; i < filas; ++i) {
             for (int j = 0; j < columnas; ++j) {
-                matriz[i][j] = rand()%10;
+                matriz[i][j] = rand()%3 + 1;
             }
         }
     }
@@ -28,22 +29,19 @@ public:
     T get(int i, int j){ return matriz[i][j]; }
     T get_filas(){ return filas; }
     T get_columnas(){ return columnas; }
+    void set(int pos_fila, int pos_columna, T valor){ matriz[pos_fila][pos_columna] = valor; }
 
-    void set(int pos_fila, int pos_columna, T valor){
-        matriz[pos_fila][pos_columna] = valor;
-    }
-
-    Matriz operator+(Matriz other) const{
+    Matriz<T> operator+(Matriz<T> other) const{
         Matriz<T> temp(filas,columnas);
         for (int i = 0; i < filas; ++i) {
-            for (int j = 0; j < columnas; ++j)
-
-                temp.set(i,j,matriz[i][j] + other.get(i,j));
+            for (int j = 0; j < columnas; ++j) {
+                temp.set(i, j, matriz[i][j] + other.get(i, j));
+            }
         }
         return temp;
     }
 
-    Matriz operator*(Matriz other) const{
+    Matriz<T> operator*(Matriz<T> other) const{
         Matriz<T> temp(filas,other.get_columnas());
         for (int i = 0; i < filas; ++i) {
             for (int j = 0; j < other.get_columnas(); ++j){
@@ -57,25 +55,18 @@ public:
         return temp;
     }
 
-    Matriz&operator=(Matriz other){
-        for (int i = 0; i < filas; ++i) {
-            for (int j = 0; j < columnas; ++j) {
-                matriz[i][j] = other.get(i,j);
-            }
-        }
-    }
-
-    bool igual_dimension(Matriz other){
+    bool igual_dimension(Matriz<T> other){
         return ((filas == other.get_filas()) && (columnas == other.get_columnas()));
     }
 
     void print_matriz(){
         for (int i = 0; i < filas; ++i){
             for (int j = 0; j < columnas; ++j)
-                cout << matriz[i][j] << "\t";
-            cout<<endl;
-        }
+                std::cout << matriz[i][j] << "\t";
+            std::cout << std::endl;
+        } std::cout << std::endl;
     }
+
 };
 
 #endif //CODE_MATRIZ_H
